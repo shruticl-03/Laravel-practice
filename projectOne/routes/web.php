fb54e1b769d11164b926d75d7776b2af664a4715
Shruthi L <Shruthi@agpl.net>
2025-03-06 11:39:23 +0530

    different route created with simple message using web php file

--- projectOne/routes/web.php
+++ projectOne/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home Page';
+});
+
+Route::get('/about', function () {
+    return 'About Page';
+});
+
+Route::get('/contact', function () {
+    return 'Contact Page';
+});
+
+Route::get('/service', function () {
+    return 'Service Page';
 });
