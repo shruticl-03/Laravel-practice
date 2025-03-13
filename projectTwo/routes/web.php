bad49c965a4498de97d99d318f9742396c7c8d2c
Shruthi L <Shruthi@agpl.net>
2025-03-13 15:03:46 +0530

    Passing Data from Route to View

--- projectTwo/routes/web.php
+++ projectTwo/routes/web.php
@@ -18,14 +18,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function(){
+Route::get('/about', function () {
     return View('aboutme');
 });
 
 // Route::view('about', 'aboutme');
 
-Route::get('admin', function(){
+Route::get('admin', function () {
     return View('admin.profile');
 });
 
 // Route::view('adminprofile', 'admin.profile');
+
+Route::get('contact', function () {
+    return view('contactme', ['name' => 'Shruti']);
+});
+
+// Route::view('contact', 'contactme', ['name' => 'Shruti']);
+
+// Route::get('contact', function () {
+//     return view('contactme', ['name' => 'Shruti', 'roll' => '003']);
+// });
+
+// Route::view('contact','contactme',['name'=>'Shruti', 'roll'=>103]);
+
+// Route::get('contact', function () {
+//     return view('contactme')->with('name', 'Shruti');
+// });
