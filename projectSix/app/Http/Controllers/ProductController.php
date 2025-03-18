bf00b83bf89bd4103cf48c7af14dad3ca957548b
Shruthi L <Shruthi@agpl.net>
2025-03-18 13:26:19 +0530

    Contact Us page

---
+++ projectSix/app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function display()
+    {
+        // $websiteName = 'ClickCart';
+        // $user = 'Admin';
+        // $userType = $user == 'Admin' ? true : false;
+        // $students = ['Shruti', 'Abhi', 'Ram', 'Shahil'];
+        // return view('product', ['wn' => $websiteName, 'userType' => $userType, 'students' => $students]);
+    }
+}
---
+++ projectSix/resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<h2>Contact Us</h2>
+<h3>Name:{{$name}}</h3>
+<p>Bangalore, Karnataka</p>
+<p>{{$pincode}}</p>
